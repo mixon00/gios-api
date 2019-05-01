7fbfaaea0da091c230bb2bdec25381a779acc4c0
Mateusz Misztoft <mateusz.misztoft@gmail.com>
2019-05-01 18:21:18 +0200

    add findById method to GIOS_API class

--- tests/GIOS_API.test.js
+++ tests/GIOS_API.test.js
@@ -11,8 +11,17 @@ describe("GIOS_API Class", () => {
     const giosAPI = new GIOS_API();
     const data = await giosAPI.findAll();
     const station = data[1];
-    expect(data.length).toBe(175);
     expect(station.getId()).toBe(117);
     expect(station.getName()).toEqual("Wrocław - Korzeniowskiego");
+  });
+
+  it("new GIOS_API().findById()", async () => {
+    fetch = jest.fn(() => new Promise(resolve => resolve()));
+    const giosAPI = new GIOS_API();
+    const data = await giosAPI.findById(842);
+    const station = data[0];
+    expect(data.length).toBe(1);
+    expect(station.getId()).toBe(842);
+    expect(station.getName()).toEqual("Ustroń, ul. Sanatoryjna 7");
   });
 });
