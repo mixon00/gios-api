90f3ba3aad3ded91ca908e8f6030e4d9b5f267c8
Mateusz Misztoft <mateusz.misztoft@gmail.com>
2019-05-04 02:18:23 +0200

    add SensorsList class

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const GIOS_API = require("./GIOS_API");
 const Station = require("./Station");
+const SensorsList = require("./SensorsList");
 const Sensor = require("./Sensor");
 
-module.exports = { GIOS_API, Station, Sensor };
+module.exports = { GIOS_API, Station, SensorsList, Sensor };
