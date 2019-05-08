059d0b56982903061f1d5b5be3dbab0f758de7d5
Mateusz Misztoft <mateusz.misztoft@gmail.com>
2019-05-09 00:30:57 +0200

    export AirQuality class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-const GIOS_API = require("./GIOS_API");
-const Station = require("./Station");
-const SensorsList = require("./SensorsList");
-const Sensor = require("./Sensor");
+const GIOS_API = require('./GIOS_API');
+const Station = require('./Station');
+const SensorsList = require('./SensorsList');
+const Sensor = require('./Sensor');
+const AirQuality = require('./AirQuality');
 
-module.exports = { GIOS_API, Station, SensorsList, Sensor };
+module.exports = { GIOS_API, Station, SensorsList, Sensor, AirQuality };
